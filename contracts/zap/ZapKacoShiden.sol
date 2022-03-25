15f03e3edee5bf5f1f2d2531ac3dd1c0c002f514
Ericssonchan <jackyaaaaa12563@gmail.com>
2022-03-25 11:38:49 +0800

    feat -> MIT License

--- contracts/zap/ZapKacoShiden.sol
+++ contracts/zap/ZapKacoShiden.sol
@@ -1,15 +1,14 @@
-// SPDX-License-Identifier: GPLv3
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
-
 
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 import "../interfaces/IPancakePair.sol";
 import "../interfaces/IPancakeRouter02.sol";
 import "../interfaces/IZap.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "../interfaces/IWETH.sol";
 
 
